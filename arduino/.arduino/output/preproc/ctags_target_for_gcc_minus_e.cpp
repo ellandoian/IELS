852ee3be639fcac1f6d587867a905c8d9f36997a
ellandoian <jjojjo3214@gmail.com>
2023-09-21 12:43:48 +0200

    k

--- arduino/.arduino/output/preproc/ctags_target_for_gcc_minus_e.cpp
+++ arduino/.arduino/output/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,1 +1,7 @@
 # 1 "C:\\Users\\ellan\\Documents\\iesl1001\\arduino\\reaksjonstid\\reaksjonstid.ino"
+void setup()
+{
+}
+void loop()
+{
+}
--- arduino/.arduino/output/sketch/reaksjonstid.ino.cpp
+++ arduino/.arduino/output/sketch/reaksjonstid.ino.cpp
@@ -1,3 +1,13 @@
 #include <Arduino.h>
 #line 1 "C:\\Users\\ellan\\Documents\\iesl1001\\arduino\\reaksjonstid\\reaksjonstid.ino"
-
+#line 1 "C:\\Users\\ellan\\Documents\\iesl1001\\arduino\\reaksjonstid\\reaksjonstid.ino"
+void setup();
+#line 4 "C:\\Users\\ellan\\Documents\\iesl1001\\arduino\\reaksjonstid\\reaksjonstid.ino"
+void loop();
+#line 1 "C:\\Users\\ellan\\Documents\\iesl1001\\arduino\\reaksjonstid\\reaksjonstid.ino"
+void setup()
+{
+}
+void loop()
+{
+}
